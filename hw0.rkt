c08090e05a469ebc047d89546295c9d20516a201
bryce-ma <lmfy0717@gmail.com>
2016-07-16 11:33:12 +0800

    hw0 done, just for warmup

--- hw0.rkt
+++ hw0.rkt
@@ -10,6 +10,7 @@
 
 (3rd-power 17)
 
+;; Part 2 — Implement 42nd-power
 (define 42nd-power
   (lambda (x)
     (let* ([3rd (3rd-power x)]
@@ -19,6 +20,7 @@
 
 (42nd-power 17)
 
+;; Part 3 — Implement plural
 (define plural
   (lambda (x)
     (let* ([last-index (- (string-length x) 1)]
@@ -30,3 +32,32 @@
 
 (plural "baby")
 (plural "fish")
+
+;; Part 4 — Implement energy-usage
+(define-type Light
+    [bulb (watts : number)
+          (technology : symbol)]
+    [candle (inches : number)])
+(define energy-usage
+  (lambda (x)
+    (type-case Light x
+      (bulb (w t) (/ (* 24 w) 1000))
+      (candle (i) 0.0))))
+
+(energy-usage (bulb 100.0 'halogen))
+(energy-usage (candle 10.0))
+
+;; Part 5 — Implement use-for-one-hour
+(define use-for-one-hour
+  (lambda (x)
+    (type-case Light x
+      (bulb (w t) (bulb w t))
+      (candle (i) (candle (if (< i 1)
+                              0
+                              (- i 1)))))))
+
+(use-for-one-hour (bulb 100.0 'halogen))
+(use-for-one-hour (candle 10.0))
+
+;; Part 6 — Handin
+;; omitted
